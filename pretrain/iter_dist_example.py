7017ffb4157b98d6137acf5e795e258500af2964
catX <shuirh2019@gmail.com>
2023-12-17 20:00:40 +0800

    add logging and output data handle process

--- pretrain/iter_dist_example.py
+++ pretrain/iter_dist_example.py
@@ -5,21 +5,27 @@ import time
 from torch.utils.data import IterableDataset, DataLoader
 import accelerate
 
+import logging
+
+logging.basicConfig(filename = './log.txt', level = 10)
 
 class MyIterDataset(IterableDataset):
     def __init__(self, n):
         super().__init__()
         self.n = n
+        state = accelerate.PartialState()
+        self.local_rank = state.process_index
     
     def __iter__(self):
         for i in range(self.n):
             time.sleep(0.2)
+            logging.info(f'Handle data {i} by Process {self.local_rank}')
             yield i
 
 def main():
     accelerator = accelerate.Accelerator()
     
-    ds = MyIterDataset(50)
+    ds = MyIterDataset(20)
     dl = DataLoader(ds, batch_size = 2)
 
     dl = accelerator.prepare(dl)
@@ -27,7 +33,7 @@ def main():
     local_rank = accelerator.process_index
 
     for batch in dl:
-        print(f'{local_rank} : {batch}')
+        logging.info(f'Process {local_rank} : {batch}')
 
 if __name__ == '__main__':
     main()
