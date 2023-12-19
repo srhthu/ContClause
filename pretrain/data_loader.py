c772bb6171be717e4628d2d1369d2cf0b284b0c7
catX <shuirh2019@gmail.com>
2023-12-19 22:21:32 +0800

    update pretrain demo

--- pretrain/data_loader.py
+++ pretrain/data_loader.py
@@ -1,5 +1,82 @@
+from pathlib import Path
 import torch
 from torch.utils.data import DataLoader, Dataset, IterableDataset
 
 from transformers import PreTrainedTokenizer
 
+class FileLoader(IterableDataset):
+    """
+    Load txt files under the folder.
+
+    The data_dir has two levels. The first level is sub dirs, the second level is txt files.
+    """
+    def __init__(self, data_dir, tokenizer, max_length):
+        super().__init__()
+        self.data_dir = data_dir
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+        self.status = {} 
+        # record the process
+        # keys: sub_dir, file_name, chunk, total
+    
+    def __iter__(self):
+        count = 0
+        for sub_dir in Path(self.data_dir).glob("*"):
+            self.status['sub_dir'] = str(sub_dir)
+            for file in sub_dir.glob("*"):
+                self.status['file_name'] = str(file)
+                doc = open(file).read()
+                for i, chunk_enc in enumerate(self.process_doc(doc)):
+                    count += 1
+                    self.status['chunk'] = i
+                    self.status['total'] = count
+                    yield chunk_enc
+                    
+    
+    def process_doc(self, doc):
+        """
+        Return the tokenization result of chunks of one document.
+        """
+        enc = self.tokenizer(
+            doc, max_length = self.max_length, 
+            truncation = True, padding = 'max_length',
+            return_overflowing_tokens = True
+        )
+        
+        ignore_keys = ['overflow_to_sample_mapping']
+
+        for chunk in zip(*[enc[k] for k in enc.keys()]):
+            enc_dict = dict(zip(enc.keys(), chunk))
+            enc_dict = {k:v for k,v in enc_dict.items() if k not in ignore_keys}
+
+            enc_dict['length'] = sum(enc_dict['attention_mask'])
+
+            if enc_dict['length'] < 2:
+                continue
+
+            yield enc_dict
+
+
+if __name__ == '__main__':
+    # Test code
+    import argparse
+    from transformers import AutoTokenizer
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir')
+    parser.add_argument('--tokenizer', default = 'gpt2')
+    args = parser.parse_args()
+
+    tk = AutoTokenizer.from_pretrained(args.tokenizer)
+    tk.add_special_tokens({'pad_token': '[PAD]'})
+
+    dl = FileLoader(args.data_dir, tk, max_length = 128)
+
+    count = 0
+    for enc_dict in dl:
+        count += 1
+        print(dl.status)
+        print(enc_dict)
+        if count > 10:
+            break
--- pretrain/demo_train_ds.py
+++ pretrain/demo_train_ds.py
@@ -5,4 +5,27 @@ Aim:
     providing a toy example
     investigating memory usage of different input seq length.
 """
+import argparse
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path')
+    parser.add_argument('--dtype', choices = ['fp16', 'bf16'], default = 'fp16')
+    # parser.add_argument()
+    args = parser.parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code = True)
+
+    torch_dtype_map = {'fp16': torch.float16, 'bf16': torch.bfloat16}
+
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_path,
+        torch_dtype = torch_dtype_map[args.dtype],
+        device_map = 'auto'
+    )
+    print(model.hf_device_map)
+
+if __name__ == '__main__':
+    main()
